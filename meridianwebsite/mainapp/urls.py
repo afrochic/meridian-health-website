67ad75fffb4b695b38bd47fa0ba34e991c8ed05a
Keachebet <terryannekea@gmail.com>
2020-05-07 20:07:29 +0300

    Creates services page initial view

--- meridianwebsite/mainapp/urls.py
+++ meridianwebsite/mainapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('services', views.services, name='services'),
+    path('dialysis', views.dialysis, name='dialysis'),
+    path('dental', views.dental, name='dental'),
+    path('physiotherapy', views.physiotherapy, name='physiotherapy'),
+    path('diagnostic', views.diagnostic, name='diagnostic'),
 ]
--- meridianwebsite/mainapp/views.py
+++ meridianwebsite/mainapp/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'mainapp/index.html')
+
+def services(request):
+    return render(request, 'mainapp/services.html')
+
+def dialysis(request):
+        return render(request, 'mainapp/dialysis.html')
+
+def dental(request):
+    return render(request, 'mainapp/dental.html')
+
+def physiotherapy(request):
+    return render(request, 'mainapp/physiotherapy.html')
+
+def nutrition(request):
+    return render(request, 'mainapp/nutrition.html')
+
+def diagnostic(request):
+    return render(request, 'mainapp/diagnostic.html')
